840f1c0014a6592852f51cdab7a1f4f44e5379a3
Bao Nguyen <g00420095@atu.ie>
2024-11-29 11:44:30 +0000

    Delete button

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -37,6 +37,11 @@ app.get('/api/movie/:id', async (req ,res)=>{
   res.json(movie);
 })
 
+app.delete('/api/movie/:id', async (req, res)=>{
+  const movie = await movieModel.findByIdAndDelete(req.params.id);
+  res.send(movie);
+})
+
 app.put('/api/movie/:id', async (req, res)=>{
   const movie = await movieModel.findByIdAndUpdate(req.params.id, req.body, {new:true});
   res.send(movie);
--- src/components/movieitem.js
+++ src/components/movieitem.js
@@ -1,12 +1,25 @@
 import { useEffect } from "react";
 import Card from 'react-bootstrap/Card';
 import { Link } from "react-router-dom";
+import axios from "axios";
+import Button from 'react-bootstrap/Button';
 
-const MovieItem = (props)=> {
+const MovieItem = (props) => {
   useEffect(() => {
     console.log("Movie Item:", props.mymovie);
   }, [props.mymovie]); // Only run this effect when the mymovie prop changes
 
+  const handleDelete = (e) => {
+    e.preventDefault();
+
+    axios.delete('http://localhost:4000/api/movie/' + props.mymovie._id)
+      .then(() => {
+        props.Reload(); // Refresh the movie list after deletion
+      })
+      .catch((error) => {
+        console.error("Error deleting movie:", error);
+      });
+  }
   return (
     <div>
       <Card>
@@ -17,7 +30,8 @@ const MovieItem = (props)=> {
             <footer>{props.mymovie.year}</footer>
           </blockquote>
         </Card.Body>
-        <Link className="btn btn-primary" to={"/edit/"+props.mymovie._id}>Edit</Link>
+        <Link className="btn btn-primary" to={"/edit/" + props.mymovie._id}>Edit</Link>
+        <Button className="btn btn-danger" onclick={handleDelete}>Delete</Button>
       </Card>
     </div>
   );
